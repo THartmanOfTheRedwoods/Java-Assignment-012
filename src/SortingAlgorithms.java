2116ec3b1631f8c4c8b2abf7c1b28c701d34727e
vathao777 <vathao777@gmail.com>
2024-04-23 16:20:27 -0700

    Finished assignment 12

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -19,7 +19,7 @@ public class SortingAlgorithms {
 
     public static void main(String[] args) {
         SortingAlgorithms sorter = new SortingAlgorithms();
-        int[] arr = {64, 25, 12, 22, 11};
+        int[] arr = {89, 2, 15, 60, 123};
         sorter.selectionSort(arr);
         System.out.println("Sorted array: " + Arrays.toString(arr));
     }
