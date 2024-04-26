9cdcda7c288d3e4e8e32dd2b500d8a4423e9bee1
nstine77 <nstine@gmail.com>
2024-04-26 16:14:27 -0700

    This felt much easier than I was expecting.

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,18 @@
 public class SortingAlgorithms {
+    public void selectionSort(int[] arr) {  // I read that arr is a commonly used abbreviation for array.
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i; // minimum element index
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // This is to swap the smallest element with the first element of the unsorted subdeck.
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
 }
