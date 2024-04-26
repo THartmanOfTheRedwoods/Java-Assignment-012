fa9f6536c927d93a4e021a6683396ed40cbc37e9
quinnmckay <quinnmckay83@gmail.com>
2024-04-26 15:12:08 -0700

    Completed Java Assignment 12

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,24 @@
 public class SortingAlgorithms {
+
+   public static void selectionSort(int[] A, int n){
+       for(int i = 0; i<n-1; i++){
+           int iMin = i;
+           for(int j = i + 1; j<n; j++){
+               if(A[j] < A[iMin])
+                   iMin = j;
+           }
+           int temp = A[i];
+           A[i] = A[iMin];
+           A[iMin] = temp;
+       }
+   }
+
+   public static void main(String[] args){
+       int[] A = {2,7,4,1,5,3};
+       selectionSort(A,6);
+       for(int i = 0; i<6; i++){
+           System.out.print(A[i] + " ");
+       }
+
+   }
 }
