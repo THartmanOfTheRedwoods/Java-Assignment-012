fb50e4060986c0fdd16cb1e45a23e4ba0cc8bb03
jli667 <JennyL137740@gmail.com>
2024-04-28 20:37:57 -0700

    Completed assignment012

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,26 @@
 public class SortingAlgorithms {
+    public static void selectionSort(int[] arr) {
+        int a = arr.length;
+        for (int i = 0; i < a-1; i++) {
+            int min = i;
+            for (int b = i+1; b < a; b++) {
+                if (arr[b] < arr[min]) {
+                    min = b;
+                }
+            }
+
+            int first = arr[min];
+            arr[min] = arr[i];
+            arr[i] = first;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {48, 24, 12, 60, 36};
+        selectionSort(arr);
+        System.out.println("Sorted array is:");
+        for (int i : arr) {
+            System.out.println(" " + i);
+        }
+    }
 }
